0f9fcbd870018d0ff05198cbe8e957c18c481924
himanshu <softeng8894@gmail.com>
2023-10-05 22:46:14 +0530

    MARUTI1

--- src/test/java/AWS_Docker.java
+++ src/test/java/AWS_Docker.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,7 +11,9 @@ public class AWS_Docker {
 	@Test(priority = 1)
 	public void AWS1() throws IOException
 	{
-      WebDriver driver = WebDriverManager.chromedriver().create();
+	  ChromeOptions co = new ChromeOptions();
+	  co.addArguments("headless");
+      WebDriver driver = WebDriverManager.chromedriver().capabilities(co).create();
       driver.get("https://www.google.com/");
       driver.manage().window().maximize();
       System.out.println("PMO INDIA");
