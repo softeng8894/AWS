ab5857b074a8421662e58659200094ee2e2d4abd
himanshu <softeng8894@gmail.com>
2023-10-06 11:39:39 +0530

    PMO101

--- src/test/java/AWS_Docker.java
+++ src/test/java/AWS_Docker.java
@@ -7,22 +7,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AWS_Docker {
 
-	@Test(priority = 1)
-	public void AWS_browser() throws IOException
-	{
-      WebDriver driver = WebDriverManager.chromedriver().create();
-      driver.get("https://www.google.com/");
-      driver.manage().window().maximize();
-      System.out.println("PMO INDIA");
-      driver.quit();
-	}
 	@Test
 	public void AWS_headless() throws IOException
 	{
-	  WebDriver driver = WebDriverManager.chromedriver().capabilities(co).create();
 	  ChromeOptions co = new ChromeOptions();
 	  co.addArguments("--headless");
 	  co.addArguments("disable-gpu");
+	  WebDriver driver = WebDriverManager.chromedriver().capabilities(co).create();
       driver.get("https://www.google.com/");
       driver.manage().window().maximize();
       System.out.println("PMO INDIA");
