a121167257ccbf9b8738b32b00e651e4e070d4a2
himanshu <softeng8894@gmail.com>
2023-10-08 15:12:00 +0530

    DEVYANI

--- src/test/java/AWS.java
+++ src/test/java/AWS.java
@@ -1,11 +1,20 @@
 import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AWS {
 
 	@Test
 	public void AWS_headless() throws IOException
 	{
+	  ChromeOptions co = new ChromeOptions();
+	  co.addArguments("--headless");
+	  WebDriver driver = WebDriverManager.chromedriver().capabilities(co).create();
+      driver.get("https://www.google.com/");
+      driver.manage().window().maximize();
       System.out.println("PMO INDIA");
+      driver.quit();
 	}
 }
--- src/test/java/AWS_Docker.java
+++ src/test/java/AWS_Docker.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.net.URL;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
